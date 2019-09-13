2895e2f7849fbd86d1bed5a5fe7189e32f1c793d
analuciabolico <analuciabolico.dev@gmail.com>
2019-09-13 09:33:27 -0300

    refactor

---
+++ src/main/java/br/edu/ifrs/poa/SistemaGerenciamentoTCC/SistemaGerenciamentoTccApplication.java
@@ -0,0 +1,13 @@
+package br.edu.ifrs.poa.SistemaGerenciamentoTCC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaGerenciamentoTccApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaGerenciamentoTccApplication.class, args);
+	}
+
+}
