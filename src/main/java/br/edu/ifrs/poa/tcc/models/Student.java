29a2ff1f01a39f89bf12065e6974eadc8e305585
edizzera <edi_fob@hotmail.com>
2019-09-26 20:49:32 -0300

    #2 "Criada classe Student"

---
+++ src/main/java/br/edu/ifrs/poa/tcc/models/Student.java
@@ -0,0 +1,10 @@
+package br.edu.ifrs.poa.tcc.models;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "students") // nome de tabela deve ser o nome da classe no plural em letras minusculas
+public class Student {
+
+}
