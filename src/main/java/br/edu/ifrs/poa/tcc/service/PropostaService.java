34d10d845246aa763848680e1d39037fc10b8ff6
analuciabolico <analuciabolico.dev@gmail.com>
2019-10-24 22:46:58 -0200

    #9

---
+++ src/main/java/br/edu/ifrs/poa/tcc/service/PropostaService.java
@@ -0,0 +1,29 @@
+package br.edu.ifrs.poa.tcc.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.edu.ifrs.poa.tcc.models.Proposta;
+import br.edu.ifrs.poa.tcc.models.Student;
+import br.edu.ifrs.poa.tcc.repositories.PropostaRepository;
+
+@Service
+public class PropostaService {
+	
+	@Autowired
+	private PropostaRepository proposta;
+
+	public PropostaService(PropostaRepository proposta) {
+		super();
+		this.proposta = proposta;
+	}
+	
+	public Proposta buscarPropostaPeloAluno(Student aluno) {
+		try {
+			return this.proposta.findByStudent(aluno);
+		} catch (Exception exception) {
+			new Exception("Erro");
+		}
+	}
+	
+}
