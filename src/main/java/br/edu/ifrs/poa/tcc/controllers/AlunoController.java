3c88740f1e0d0031c11f8c4fdf2cd6dc064fcf34
anakettl <anakettl@gmail.com>
2019-11-28 21:38:18 -0300

    Criada rotas de visualizar, atualizar e deletar

--- src/main/java/br/edu/ifrs/poa/tcc/controllers/AlunoController.java
+++ src/main/java/br/edu/ifrs/poa/tcc/controllers/AlunoController.java
@@ -9,6 +9,7 @@ import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -68,5 +69,20 @@ public class AlunoController {
 			return "error";
 		}
 	}
+	
+	@GetMapping("{id}")
+	public ModelAndView ver(@PathVariable("id") Integer id) {
+		ModelAndView model = new ModelAndView("aluno/create");
+		try {
+			Aluno aluno = this.alunos.aluno(id);
+			model.addObject("aluno", aluno);
+			return model;
+		} catch (Exception exception) {
+			model.addObject("erro", exception.getMessage());
+			model.setViewName("alunos");
+			return model;
+		}
+	}
+	
 	
 }
--- src/main/java/br/edu/ifrs/poa/tcc/repositories/AlunoRepository.java
+++ src/main/java/br/edu/ifrs/poa/tcc/repositories/AlunoRepository.java
@@ -1,12 +1,14 @@
 package br.edu.ifrs.poa.tcc.repositories;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import br.edu.ifrs.poa.tcc.models.Aluno;
 
 public interface AlunoRepository extends JpaRepository<Aluno, Integer> {
 
-	Aluno findByMatricula(Long matricula);
+	Optional<Aluno> findById(Integer id);
 	
 	
 }
