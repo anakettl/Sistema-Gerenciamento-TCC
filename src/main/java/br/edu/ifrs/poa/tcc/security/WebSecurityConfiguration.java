d70226d1f6360f86247a0698a20e089960ae26cf
analuciabolico <analuciabolico.dev@gmail.com>
2019-11-29 21:26:25 -0300

    adicionado permissoes no path do console do banco em memoria H2 e reajustado autorizacoes

--- src/main/java/br/edu/ifrs/poa/tcc/security/WebSecurityConfiguration.java
+++ src/main/java/br/edu/ifrs/poa/tcc/security/WebSecurityConfiguration.java
@@ -3,6 +3,7 @@ package br.edu.ifrs.poa.tcc.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -11,11 +12,16 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(securedEnabled = true, prePostEnabled = true)
 public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
 
 	@Autowired
 	AuthService servico;
 
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(servico).passwordEncoder(new BCryptPasswordEncoder());
+    }
 	
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -31,8 +37,6 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .and()
             .formLogin()
                 .loginPage("/login")
-                .usernameParameter("login")
-                .passwordParameter("senha")
                 .failureUrl("/login?error=1")
                 .permitAll()
                 .and()
@@ -41,11 +45,8 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .logoutSuccessUrl("/login?logout")
                 .invalidateHttpSession(true)
                 .permitAll();
-    }
-
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-    	auth.userDetailsService(servico).passwordEncoder(new BCryptPasswordEncoder());
+        http.csrf().ignoringAntMatchers("/h2/**");
+        http.headers().frameOptions().sameOrigin();
     }
     
 }
