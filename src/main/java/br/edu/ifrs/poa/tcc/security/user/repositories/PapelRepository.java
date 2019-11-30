99e37b6b98c79b74ef95c95df2c742d65f766a31
analuciabolico <analuciabolico.dev@gmail.com>
2019-11-30 20:33:37 -0300

    adicionando controller de cadastro de usuario

---
+++ src/main/java/br/edu/ifrs/poa/tcc/security/user/repositories/PapelRepository.java
@@ -0,0 +1,8 @@
+package br.edu.ifrs.poa.tcc.security.user.repositories;
+
+import br.edu.ifrs.poa.tcc.security.user.Papel;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PapelRepository extends JpaRepository<Papel, Integer> {
+
+}
