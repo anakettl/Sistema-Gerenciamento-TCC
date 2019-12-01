c2ba763bad6c88f1ab7e31620560cb8b8fe7f7b3
analuciabolico <analuciabolico.dev@gmail.com>
2019-12-01 19:24:54 -0300

    cria metodo para procurar proposta por id

--- src/main/java/br/edu/ifrs/poa/tcc/repositories/PropostaRepository.java
+++ src/main/java/br/edu/ifrs/poa/tcc/repositories/PropostaRepository.java
@@ -5,13 +5,16 @@ import br.edu.ifrs.poa.tcc.models.Proposta;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface PropostaRepository extends JpaRepository<Proposta, Integer> {
 
-    //sempre depois do findby vai o nome do atributo que deve ser encontrado
-    Proposta findByAutor(Aluno autor);
 
     List<Proposta> findAll();
+
+    Optional<Proposta> findById(Long id);
+
+    Proposta findByAutor(Aluno autor);
 
 
 }
