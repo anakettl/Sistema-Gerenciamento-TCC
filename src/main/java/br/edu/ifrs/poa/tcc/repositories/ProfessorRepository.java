58b927d290b661bfd8c0a43215ac1d0dec11c658
analuciabolico <analuciabolico.dev@gmail.com>
2019-12-01 15:57:01 -0300

    cria metodo no repositorio para trazer todos os professores

--- src/main/java/br/edu/ifrs/poa/tcc/repositories/ProfessorRepository.java
+++ src/main/java/br/edu/ifrs/poa/tcc/repositories/ProfessorRepository.java
@@ -4,7 +4,13 @@ import br.edu.ifrs.poa.tcc.models.Professor;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ProfessorRepository extends JpaRepository<Professor, Integer> {
+
+    @Override
+    List<Professor> findAll();
+
 
 }
