a2bf145110a4b5688d94067b8521f66df9723767
anakettl <anakettl@gmail.com>
2021-04-07 17:35:05 -0300

    add configuration to build tests

---
+++ src/test/java/AddConfigTest.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class AddConfigTest {
+
+    @Test
+    void testConfiguration(){
+        assertEquals(1, 1);
+    }
+}
